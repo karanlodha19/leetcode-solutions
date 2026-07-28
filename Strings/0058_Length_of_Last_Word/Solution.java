bbdb231b46855985f75a3b93200863253340e6a5
karanlodha19 <108725533+karanlodha19@users.noreply.github.com>
2026-07-28 18:54:42 +0530

    0058_Solution.java

--- Strings/0058_Length_of_Last_Word/Solution.java
+++ Strings/0058_Length_of_Last_Word/Solution.java
@@ -7,12 +7,10 @@ class Solution {
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
-
         while (i >= 0 && s.charAt(i) != ' ') {
             count++;
             i--;
         }
-
         return count;
     }
 }
