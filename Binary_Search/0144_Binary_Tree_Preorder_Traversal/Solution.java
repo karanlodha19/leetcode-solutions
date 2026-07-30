fb3e99559b781b9996068d1ba8648747ccf500d7
karanlodha19 <108725533+karanlodha19@users.noreply.github.com>
2026-07-30 09:48:25 +0530

    0144_Solution.java

---
+++ Binary_Search/0144_Binary_Tree_Preorder_Traversal/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        preorder(root);
+        return ans;
+    }
+
+    private void preorder(TreeNode root) {
+        if (root == null) return;
+
+        ans.add(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+}
